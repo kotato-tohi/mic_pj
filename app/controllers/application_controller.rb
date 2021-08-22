06024b599b41b81d776179cfc2dd05fa17780f60
土肥康太郎 <tohi@doikoutarounoMacBook-Pro.local>
2021-08-22 23:43:06 +0900

    ログイン機能のエラー修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+    # before_action :authenticate_user!
+    def after_sign_in_path_for(resource)
+        test_path
+      end
+    
+      def after_sign_out_path_for(resource)
+        top_path
+      end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # root 'top#index'
-  get '/', to: 'top#index'
+  get '/', to: 'top#index', as: "top"
   get '/test', to: 'top#test'
   
 end
